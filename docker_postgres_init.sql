1124a94d9ab51ce8377e78078c36ad6b75d81d46
eum602 <eum602@gmail.com>
2023-06-08 09:48:27 -0500

    fix: typos

--- docker_postgres_init.sql
+++ docker_postgres_init.sql
@@ -1,10 +1,10 @@
 CREATE USER docker WITH PASSWORD 'password' CREATEDB;
 
-CREATE DATABASE laccpass_identity_development
+CREATE DATABASE lacpass_identity_development
 WITH OWNER = docker
 CONNECTION LIMIT = -1;
 
-CREATE DATABASE laccpass_identity
+CREATE DATABASE lacpass_identity
 WITH OWNER = docker
 CONNECTION LIMIT = -1;
 
